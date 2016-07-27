3b0cfee2cba747e1a7216c2ebc0f21349cf91bd5
bguillon-td <bguillon@talend.com>
2016-07-27 17:40:23 +0200

    Make it work with Akka streams kafka

--- src/test/java/org/talend/schema/TestConsumer.java
+++ src/test/java/org/talend/schema/TestConsumer.java
@@ -1,57 +1,48 @@
 package org.talend.schema;
 
+import akka.Done;
+import akka.actor.ActorSystem;
+import akka.kafka.ConsumerSettings;
+import akka.kafka.Subscriptions;
+import akka.kafka.javadsl.Consumer;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.concurrent.CompletionStage;
 
 public class TestConsumer {
 
-
     @Test
     public void testConsumer() throws Exception {
-        KafkaConsumer<String, String> consumer = createConsumer("localhost:9092", "schemamanager", 100);
-        String topic = "_schemas";
-        //consumer.subscribe(Collections.singleton(topic));
-        assignPartitions(consumer, topic);
 
-        while(true){
-            ConsumerRecords<String, String> messages = consumer.poll(100);
-            messages.forEach(message -> {
-                System.out.println("Key : " + message.key() + ", value : " + message.value());
-            });
-        }
+        ActorSystem system = ActorSystem.create("kafka-consumer");
 
+        ActorMaterializer materializer = ActorMaterializer.create(system);
 
+        final ConsumerSettings<String, String> consumerSettings =
+        ConsumerSettings.create(system, new StringDeserializer(), new StringDeserializer())
+                .withBootstrapServers("localhost:9092")
+                .withGroupId("group1")
+                .withClientId("Client")
+                .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
+                .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        Source<ConsumerRecord<String, String>, Consumer.Control> source = Consumer.plainSource(consumerSettings, Subscriptions.topics("_schemas"));
+
+        Sink<ConsumerRecord<String, String>, CompletionStage<Done>> sink = Sink.foreach(this::handleMessage);
+
+        source.runWith(sink, materializer);
+
+        system.awaitTermination();
     }
 
-    private static void assignPartitions(KafkaConsumer<String, String> consumer, String topic) {
-        List<TopicPartition> topicPartitions = consumer.partitionsFor(topic).stream().map(partitionInfo -> new TopicPartition(partitionInfo.topic(), partitionInfo.partition())).collect(Collectors.toList());
-        consumer.assign(topicPartitions);
-        consumer.seekToBeginning(topicPartitions);
-    }
-
-
-
-    private KafkaConsumer<String, String> createConsumer(String broker, String group, int maxPollRecords) throws Exception {
-        Map<String, Object> conf = new HashMap<>();
-        conf.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, broker);
-        conf.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        conf.put(ConsumerConfig.GROUP_ID_CONFIG, group);
-        conf.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        conf.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        conf.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        conf.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
-        return new KafkaConsumer<>(conf);
-
+    private void handleMessage(ConsumerRecord<String, String> message){
+        System.out.println("Got a message: " + message.key() + " / " + message.value());
     }
 
 
